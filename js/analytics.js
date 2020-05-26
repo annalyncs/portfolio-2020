7d021a331f84b4cb2c4c91b7688c00009a27069d
annalyncs <annalyn.c.s@gmail.com>
2020-05-25 21:55:02 -0700

    update analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function($){
     $('.email-link').on('click', function(){
-        ga('send', 'event', 'contact', 'click/touch', 'email' );
+        gtag('send', 'event', 'contact', 'click/touch', 'email' );
     })
 
     $('.contact-linkedin a').on('click', function(){
-        ga('send', 'event', 'social', 'click/touch', 'linkedin' );
+        gtag('send', 'event', 'social', 'click/touch', 'linkedin' );
     })
 
     $('.contact-dribbble a').on('click', function(){
-        ga('send', 'event', 'social', 'click/touch', 'dribbble' );
+        gtag('send', 'event', 'social', 'click/touch', 'dribbble' );
     })
 
     $('.contact-github a').on('click', function(){
-        ga('send', 'event', 'social', 'click/touch', 'github' );
+        gtag('send', 'event', 'social', 'click/touch', 'github' );
     })
 })
